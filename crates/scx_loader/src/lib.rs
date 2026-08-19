9ddae559367c2e3949ae9edc271b31d1506bc961
Piotr Gorski <lucjan.lucjanov@gmail.com>
2026-08-19 12:23:47 +0200

    scx_loader: Add chaos scheduler
    Signed-off-by: Piotr Gorski <lucjan.lucjanov@gmail.com>

--- crates/scx_loader/src/lib.rs
+++ crates/scx_loader/src/lib.rs
@@ -48,6 +48,8 @@ pub enum SupportedSched {
     Forge,
     #[serde(rename = "scx_mlfq")]
     MLFQ,
+    #[serde(rename = "scx_chaos")]
+    Chaos,
 }
 
 impl FromStr for SupportedSched {
@@ -58,6 +60,7 @@ impl FromStr for SupportedSched {
             "scx_beerland" => Ok(SupportedSched::Beerland),
             "scx_bpfland" => Ok(SupportedSched::Bpfland),
             "scx_cake" => Ok(SupportedSched::Cake),
+            "scx_chaos" => Ok(SupportedSched::Chaos),
             "scx_cosmos" => Ok(SupportedSched::Cosmos),
             "scx_flash" => Ok(SupportedSched::Flash),
             "scx_flow" => Ok(SupportedSched::Flow),
@@ -87,6 +90,7 @@ impl From<SupportedSched> for &str {
             SupportedSched::Beerland => "scx_beerland",
             SupportedSched::Bpfland => "scx_bpfland",
             SupportedSched::Cake => "scx_cake",
+            SupportedSched::Chaos => "scx_chaos",
             SupportedSched::Cosmos => "scx_cosmos",
             SupportedSched::Flash => "scx_flash",
             SupportedSched::Flow => "scx_flow",
